b35f3de67596b861c184e1876c31420d7aabe5f9
KoushikReddyB <2200090018@kluniversity.in>
2024-12-07 13:53:27 +0530

    Def Changes

--- frontend/src/Interceptor/AxiosInterceptor.tsx
+++ frontend/src/Interceptor/AxiosInterceptor.tsx
@@ -3,7 +3,7 @@ import { removeUser } from "../Slices/UserSlice";
 import { removeJwt } from "../Slices/JwtSlice";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://zakishi-backend.onrender.com/'
+    baseURL: 'http://localhost:8080'
 });
 
 axiosInstance.interceptors.request.use(
